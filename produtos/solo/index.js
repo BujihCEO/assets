5ee098dcce8e2d1ed87970c47e7eb7c347aecde0
BujihCEO <shop.bujih@gmail.com>
2025-05-19 00:21:22 -0300

    Create index.js

---
+++ produtos/solo/index.js
@@ -0,0 +1,37 @@
+var colorMenu = [
+    {
+        color: '#000000',
+    },
+];
+
+var iniciar = {
+    produtos: ['Camiseta'],
+    add: {
+        Frente: {
+            attrs: [
+                {x: 0, y: 0, width: 877, height: 1240},
+            ],
+            add: [
+                {
+                    tittle: 'Foto',
+                    upload: {
+                        type: 'svg',
+                        attrs: {
+                            noEdit: {
+                                jsColor: [
+                                    { attrs: ['color', 'borderColor'], id: 1 },
+                                ],
+                            },
+                            edit: {
+                                borderWidth: 0,
+                            }
+                        },
+                        add: [
+                            {width: 877, height: 1240},
+                        ]
+                    },
+                },
+            ]
+        },
+    }
+};
